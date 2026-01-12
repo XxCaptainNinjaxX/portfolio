c354b2ebac4251f9fd7f623350995cee268ce546
XxCaptainNinjaxX <captainninjaflash@gmail.com>
2026-01-11 21:15:15 -0500

    fixed off/on switch, added background pic

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home/Home";
 import Developer from "./pages/Developer/Developer";
 import Musician from "./pages/Musician/Musician";
 import SplashCursor from "./components/background/splashCursor";
+import background from "./assets/images/four.jpg";
 
 export default function App() {
   const [cursorOn, setCursor] = useState(true);
@@ -14,8 +15,31 @@ export default function App() {
   };
 
   return (
-    <div>
-      {cursorOn && <SplashCursor />}
+    <div
+      style={{ position: "relative", minHeight: "100vh", overflow: "hidden" }}
+    >
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          zIndex: -2,
+        }}
+      />
+
+      <div
+        style={{
+          opacity: cursorOn ? 1 : 0,
+          pointerEvents: "none",
+        }}
+      >
+        <SplashCursor />
+      </div>
 
       <Header isSplashOn={cursorOn} toggleSplash={toggleCursor} />
 
