4455789c13e2f060154c9da9bdf5307bca11591e
XxCaptainNinjaxX <captainninjaflash@gmail.com>
2026-01-31 18:16:14 -0500

    Cleaned up Home.css to make it look better overall on web + mobile

--- my-portfolio/src/components/background/Animation.tsx
+++ my-portfolio/src/components/background/Animation.tsx
@@ -10,7 +10,7 @@ export default function Reveal({ children, className = "" }: RevealProps) {
   const fadeInUp = {
     initial: { opacity: 0, y: 100 },
     whileInView: { opacity: 1, y: 0 },
-    viewport: { once: false },
+    viewport: { once: false, amount: 0.5 },
     transition: { duration: 1.0 },
   };
 
