30ef9559bdcc15e1b361501616d1f6a2ddad9ffb
XxCaptainNinjaxX <captainninjaflash@gmail.com>
2026-02-04 01:10:04 -0500

    fixed minor issues stoping react build to publish

--- my-portfolio/src/components/PageHeader/PageHeader.tsx
+++ my-portfolio/src/components/PageHeader/PageHeader.tsx
@@ -1,5 +1,4 @@
 import "./PageHeader.css";
-import Card from "../Card/Card.tsx";
 
 interface HeaderProp {
   title: string;
@@ -9,56 +8,6 @@ export default function PageHeader(props: HeaderProp) {
     <div>
       <main className="main-content">
         <h2> {props.title}</h2>
-        {/* <div className="video-grid">
-          <a
-            href="https://www.youtube.com/watch?v=4Nhz4QswAV0"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="video-card"
-          >
-            <div className="video-thumbnail">
-              <img
-                src="https://i.ytimg.com/vi/4Nhz4QswAV0/hqdefault.jpg"
-                alt="Episode 2"
-              />
-            </div>
-
-            <div className="video-info">
-              <span className="video-tag">
-                Preseason NBA Eastern Confrence{" "}
-              </span>
-              <span className="video-views">46:01</span>
-            </div>
-          </a>
-          <a href="#" className="video-card">
-            <div className="video-thumbnail">Coming soon!</div>
-            <div className="video-info">
-              <span className="video-tag">-----</span>
-              <span className="video-views">.... </span>
-            </div>{" "}
-          </a>
-          <a href="#" className="video-card">
-            <div className="video-thumbnail">Coming soon!</div>
-            <div className="video-info">
-              <span className="video-tag">-----</span>
-              <span className="video-views">.... </span>
-            </div>{" "}
-          </a>
-          <a href="#" className="video-card">
-            <div className="video-thumbnail">Coming soon!</div>
-            <div className="video-info">
-              <span className="video-tag">-----</span>
-              <span className="video-views">.... </span>
-            </div>{" "}
-          </a>
-          <a href="#" className="video-card">
-            <div className="video-thumbnail">Coming soon!</div>
-            <div className="video-info">
-              <span className="video-tag">-----</span>
-              <span className="video-views">.... </span>
-            </div>{" "}
-          </a>
-        </div> */}
       </main>
     </div>
   );
